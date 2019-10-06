2ff07cadbbf7130e8b262055c952cbe38d818633
Nithin <nithin2889@gmail.com>
2019-10-06 13:18:06 +0530

    separation of concerns to use hooks for all the background work

--- src/main/java/com/learncucumber/stepdefinitions/hooks/Hooks.java
+++ src/main/java/com/learncucumber/stepdefinitions/hooks/Hooks.java
@@ -1,5 +1,26 @@
 package com.learncucumber.stepdefinitions.hooks;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Hooks {
 
+  public static WebDriver driver;
+
+  @Before
+  public void setup() {
+    // Initiate the WebDriver
+    System.setProperty(
+        "webdriver.chrome.driver",
+        "C:\\Users\\Nithin\\Downloads\\DevArea\\Dev "
+            + "Softwares\\Selenium\\Drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");
+    driver = new ChromeDriver();
+  }
+
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
 }
