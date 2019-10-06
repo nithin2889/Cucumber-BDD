07645e6b3e0054c48896ac9e20b7b60f908f6829
Nithin <nithin2889@gmail.com>
2019-10-06 13:16:54 +0530

    glue code for the feature files

--- src/main/java/com/learncucumber/stepdefinitions/LoginStepDefs.java
+++ src/main/java/com/learncucumber/stepdefinitions/LoginStepDefs.java
@@ -2,30 +2,19 @@ package com.learncucumber.stepdefinitions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
+import com.learncucumber.stepdefinitions.hooks.Hooks;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginStepDefs {
   // Use Cucumber to bind the feature file steps to the Java code.
   // Glue Code - Step Definitions or Step Implementations
 
-  private WebDriver driver;
-
-  @Before
-  public void setup() {
-    // Initiate the WebDriver
-    System.setProperty(
-        "webdriver.chrome.driver",
-        "C:\\Users\\Nithin\\Downloads\\DevArea\\Dev "
-            + "Softwares\\Selenium\\Drivers\\Chrome\\chromedriver_win32\\chromedriver.exe");
-    driver = new ChromeDriver();
-  }
+  private WebDriver driver = Hooks.driver;
 
   // Given the user is on the login page
   @Given(value = "^the user is on the login page$")
@@ -48,12 +37,35 @@ public class LoginStepDefs {
   @Then(value = "^the user should be able to view their account balance$")
   public void userShouldBeAbleToViewAccountBalance() {
     System.out.println("3: the user should be able to view their account balance");
-    assertThat(driver.findElement(By.id("conf_message")).getText())
-        .isEqualTo("Logged in successfully");
+    String bodyText = driver.findElement(By.xpath("html/body")).getText();
+    String message = "Logged in successfully";
+    assertThat(bodyText).contains(message);
   }
 
-  @After
-  public void tearDown() {
-    driver.quit();
+  // When the user enters bad credentials
+  @When(value = "^the user enters bad credentials$")
+  public void userEntersBadCredentials() {
+    System.out.println("2: the user user enters bad credentials");
+    driver.findElement(By.id("MainContent_txtUserName")).sendKeys("nithin@gmail.com");
+    driver.findElement(By.id("MainContent_txtPassword")).sendKeys("password123");
+    driver.findElement(By.id("MainContent_btnLogin")).click();
+  }
+
+  // Then the user should be not able to login
+  @Then(value = "^the user should be not able to login$")
+  public void userShouldNotBeAbleToLogin() {
+    System.out.println("3: the user should be not able to login");
+    String bodyText = driver.findElement(By.xpath("html/body")).getText();
+    String message = "Logged in successfully";
+    assertThat(bodyText).doesNotContain(message);
+  }
+
+  // And the user should get an invalid login message
+  @And(value = "the user should get an invalid login message")
+  public void userShouldGetAnInvalidLoginMessage() {
+    System.out.println("4: the user should get an invalid login message");
+    String bodyText = driver.findElement(By.xpath("html/body")).getText();
+    String message = "Invalid password, try again!";
+    assertThat(bodyText).contains(message);
   }
 }
